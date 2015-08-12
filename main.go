9eceb4505d41c7de4c285d90c0a4cd05f5b97fa3
othree <othree@gmail.com>
2015-08-12 17:38:04 +0800

    Read and write ws message

--- main.go
+++ main.go
@@ -1,10 +1,54 @@
 package main;
 
 import (
+  "log"
   "fmt"
+  "net/http"
   "github.com/golang/protobuf/proto"
+  "github.com/gorilla/websocket"
   "./message"
 )
+
+var data []byte
+
+var upgrader = websocket.Upgrader{
+  ReadBufferSize:  1024,
+  WriteBufferSize: 1024,
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  conn, err := upgrader.Upgrade(w, r, nil)
+  if err != nil {
+    return
+  }
+  for {
+    _, _, err := conn.ReadMessage()
+    if err != nil {
+      return
+    }
+    fmt.Println("read done")
+    fmt.Println("write %s", data)
+    if err = conn.WriteMessage(websocket.BinaryMessage, data); err != nil {
+      fmt.Println("err %s", err)
+      return
+    }
+    fmt.Println("write done")
+  }
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.Error(w, "Not found", 404)
+    return
+  }
+  if r.Method != "GET" {
+    http.Error(w, "Method not allowed", 405)
+    return
+  }
+  w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  w.Write([]byte(homeHTML))
+}
+
 
 func main() {
 
@@ -20,7 +64,41 @@ func main() {
 
   fmt.Println(msg.GetAuthor().GetName() + ": " +msg.GetText())
 
-  data, _ := proto.Marshal(msg)
+  data, _ = proto.Marshal(msg)
 
   fmt.Println("%s", data)
+
+  http.HandleFunc("/", serveHome)
+  http.HandleFunc("/ws", handler)
+  if err := http.ListenAndServe("127.0.0.1:1337", nil); err != nil {
+    log.Fatal(err)
+  }
 }
+
+const homeHTML = `<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>WebSocket Example</title>
+    </head>
+    <body>
+        <script type="text/javascript">
+            (function() {
+                var data = document.getElementById("fileData");
+                var conn = new WebSocket("ws://127.0.0.1:1337/ws");
+                conn.onopen = function () {
+                  console.log("Opening a connection...");
+                  conn.send('MSG');
+                  conn.binaryType = "arraybuffer";
+                }
+                conn.onclose = function(evt) {
+                  data.textContent = 'Connection closed';
+                }
+                conn.onmessage = function(evt) {
+                    console.log('file updated');
+                    data.textContent = evt.data;
+                }
+            })();
+        </script>
+    </body>
+</html>
+`
