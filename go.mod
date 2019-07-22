6dc719aacac876e284e8ab7611cf6ded4839a8ad
michael k <mking@arikel.net>
2019-07-22 08:44:12 -0400

    feat(go112): update for go modules / go 2019

---
+++ go.mod
@@ -0,0 +1,8 @@
+module golang-protobuf-websocket
+
+go 1.12
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/gorilla/websocket v1.4.0
+)
--- main.go
+++ main.go
@@ -1,63 +1,61 @@
-package main;
+package main
 
 import (
-  "log"
-  "fmt"
-  "net/http"
-  "github.com/golang/protobuf/proto"
-  "github.com/gorilla/websocket"
-  "./message"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/websocket"
+	"golang-protobuf-websocket/message"
+	"log"
+	"net/http"
 )
 
 var data []byte
 
 var upgrader = websocket.Upgrader{
-  ReadBufferSize:  1024,
-  WriteBufferSize: 1024,
-  CheckOrigin: func(r *http.Request) bool { return true },
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  conn, err := upgrader.Upgrade(w, r, nil)
-  if err != nil {
-    return
-  }
-  for {
-    _, _, err := conn.ReadMessage()
-    if err != nil {
-      return
-    }
-    fmt.Println("read done")
-    fmt.Println("write %s", data)
-    if err = conn.WriteMessage(websocket.BinaryMessage, data); err != nil {
-      fmt.Println("err %s", err)
-      return
-    }
-    fmt.Println("write done")
-  }
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		fmt.Println("read done")
+		fmt.Printf("write %s\n", data)
+		if err = conn.WriteMessage(websocket.BinaryMessage, data); err != nil {
+			fmt.Printf("err %s\n", err)
+			return
+		}
+		fmt.Println("write done")
+	}
 }
 
 func main() {
 
-  msg := &message.Message{
-    Id: proto.Int32(17),
-    Author: &message.Message_Person{
-      Id: proto.Int32(1),
-      Name:  proto.String("othree"),
-    },
-    Text:  proto.String("Hi, this is message."),
-  }
+	msg := &message.Message{
+		Id: proto.Int32(17),
+		Author: &message.Message_Person{
+			Id:   proto.Int32(1),
+			Name: proto.String("othree"),
+		},
+		Text: proto.String("Hi, this is message."),
+	}
 
+	fmt.Println(msg.GetAuthor().GetName() + ": " + msg.GetText())
 
-  fmt.Println(msg.GetAuthor().GetName() + ": " +msg.GetText())
+	data, _ = proto.Marshal(msg)
 
-  data, _ = proto.Marshal(msg)
+	fmt.Println("%s", data)
 
-  fmt.Println("%s", data)
-
-  http.HandleFunc("/ws", handler)
-  if err := http.ListenAndServe("127.0.0.1:1337", nil); err != nil {
-    log.Fatal(err)
-  }
+	http.HandleFunc("/ws", handler)
+	if err := http.ListenAndServe("127.0.0.1:1337", nil); err != nil {
+		log.Fatal(err)
+	}
 }
-
